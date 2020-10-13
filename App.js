c7436fd0e8b53a2afd92e660a5279168ce7823df
movingbro <54231339+blivinme@users.noreply.github.com>
2020-10-13 23:20:33 +0900

    10/13

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hello from './src/Hello';
+
+function App() {
+    return (
+        <div>
+            <Hello />
+        </div>
+    );
+}
+
+export default App;
